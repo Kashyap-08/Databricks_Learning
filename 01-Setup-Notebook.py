2104c8f0973523f1400944b9a2729c0d29231b09
Kashyap-08 <Kashyap-08>
2024-07-07 09:34:54 +0000

    initial commit

---
+++ 01-Setup-Notebook.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Setup Notebook
+# MAGIC
+# MAGIC This notebook will have script for setting up the repo in databricks.
+
+# COMMAND ----------
+
+
